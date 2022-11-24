2bdcc47f7a9185eb0a44e4bd6120d866591e2d91
Vladislav <vladislav_chernikov94@mail.ru>
2022-11-24 16:53:44 +0700

    hw2

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -5,3 +5,16 @@
 34, 5 -> не кратно, остаток 4
 16, 4 -> кратно
 */
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число: ");
+int number = new int();
+number = Convert.ToInt32(Console.ReadLine());
+number = number % 100;
+int result = number / 10;
+Console.WriteLine(result);
